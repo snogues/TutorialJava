8c36f029b0a71ca9288dce6adcac45629c71bbe0
snogues <snogues55@gmail.com>
2016-03-06 13:28:18 +0100

    111

--- TutorialJava/src/Main.java
+++ TutorialJava/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		//Cambio de mierda
 		//Cambio de mierda 2
 		int i=0;
-		for (int j=0; i<2;i++){
+		for (int j=0; j<10;j++){
 			System.out.println("blabla");			
 		}
 		//miaheiaheai
