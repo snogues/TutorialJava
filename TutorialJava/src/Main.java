d95ffa0ff0824c6e75b7ccf4d5c66b3de6a55902
snogues <snogues55@gmail.com>
2016-03-06 13:19:08 +0100

    sadas

--- TutorialJava/src/Main.java
+++ TutorialJava/src/Main.java
@@ -7,7 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		//Cambio de mierda
 		//Cambio de mierda 2
-		System.out.println("blabla");
+		for (int i =0; i<2;i++){
+			System.out.println("blabla");			
+		}
 		//miaheiaheai
 	}
 
